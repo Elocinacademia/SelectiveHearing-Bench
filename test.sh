c36a58c2b312d92ad5b3f586718190a448ffab88
Brian Sun <gs534@cam.ac.uk>
2026-02-17 09:13:23 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
-
-pip install soundfile
-pip install numpy==1.26.4
-pip install qwen-omni-utils
-
 # --selective true \
 python inference.py \
     --datapath dataset/all_questions_descfilter_bg.json \
